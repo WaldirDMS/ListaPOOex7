3c0d75472a12a6ee9bffde3efdbc569cc2072bd6
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2020-11-27 19:02:42 -0300

    Add files via upload

---
+++ ListaPOOex7/ListaPOOex7/Dolar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaPOOex7
+{
+    class Dolar
+    {
+        private double dol;
+        private double real;
+        private double cot;
+
+        public void setCot(double p)
+        {
+            cot = p;
+        }
+
+        public void setDol(double p)
+        {
+            dol = p;
+        }
+
+        public double getCot()
+        {
+            return cot;
+        }
+
+        public double getDol()
+        {
+            return dol;
+        }
+
+        public double getReal()
+        {
+            return real;
+        }
+
+        public void converterMoeda()
+        {
+            real = dol * cot;
+        }
+    }
+}
---
+++ ListaPOOex7/ListaPOOex7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ListaPOOex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolar dolar;
+
+            dolar = new Dolar();
+
+            Console.Write("Informe a cotação ");
+            dolar.setCot(double.Parse(Console.ReadLine()));
+
+            Console.Write("Informe o valor em dolares ");
+            dolar.setDol(double.Parse(Console.ReadLine()));
+
+            dolar.converterMoeda();
+
+            Console.WriteLine("O valor {0} em dolares é correspontende a {1} reais", dolar.getDol(), dolar.getReal());
+        }
+    }
+}
